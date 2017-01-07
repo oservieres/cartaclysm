9e5f0afedf83d36620658dfc4d6d138396cbf48d
Olivier Servières <olivier@servieres.info>
2017-01-07 18:11:03 +0100

    Improve scene setup

---
+++ Assets/ResourcesPreloader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcesPreloader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Resources.LoadAll ("/");
+	}
+
+}
